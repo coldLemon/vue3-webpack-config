d41af08e8907b84c9bc4c81e960ad48823dc9af8
liweiqiang <liweiqiang@green.com.cn>
2022-01-06 11:05:47 +0800

    'fix:更新打包配置'

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,54 +1,66 @@
 'use strict'
 
 const webpack = require('webpack')
-const {merge} = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const webpackBaseConfig = require('./webpack.config')
 const path = require('path')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
-const SpeedMesurePlugin = require('speed-measure-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SpeedMesurePlugin = require('speed-measure-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-var portfinder = require("portfinder");
+var portfinder = require('portfinder')
 
 const smp = new SpeedMesurePlugin()
 
-const devConfig = merge(webpackBaseConfig,{
+const devConfig = merge(webpackBaseConfig, {
   mode: 'development',
-  output:{
+  output: {
     // 在 bundle.js 中引入项目所包含模块的注释信息。
     pathinfo: true
   },
-  devtool:'eval-cheap-module-source-map', //webpack5对顺序更为严格
+  devtool: 'eval-cheap-module-source-map', //webpack5对顺序更为严格
   cache: true,
   // 缓存模块，避免在未更改时重建它们。
   //如果 cache 被启用，并且此模块的来自 node_modules，则值为 true，否则为 false
-  module:{
-    unsafeCache:true,
+  module: {
+    unsafeCache: false
   },
 
-  devServer:{
-    //会把打包的东西放到内存里面  
+  devServer: {
+    //会把打包的东西放到内存里面
     // 告诉 webpack-dev-server 搭建服务器的时候从哪里获取静态文件
     // 默认情况下，将使用当前工作目录作为提供静态文件的目录
-    contentBase: path.join(__dirname, 'dist'), // since we use CopyWebpackPlugin. 
+    contentBase: path.join(__dirname, 'dist'), // since we use CopyWebpackPlugin.
+    host: 'localhost',
     port: 8088,
-    open:true,
-    hot:true,   //模块热更新，不会直接刷新浏览器，影响已经渲染的js模块
-    hotOnly:true,//就算没渲染好，也不直接刷新
-    compress: true,  // 搭建的开发服务器启动 gzip 压缩
-    // proxy: , 代理
+    open: true,
+    hot: true, //模块热更新，不会直接刷新浏览器，影响已经渲染的js模块
+    hotOnly: true, //就算没渲染好，也不直接刷新
+    compress: true, // 搭建的开发服务器启动 gzip 压缩
+    //https://webpack.docschina.org/configuration/dev-server/#devserverproxy
+    proxy: {
+      '/': {
+        //  target: 'http://192.168.166.50:8986/', //SYH
+        target: 'http://192.168.168.9:14402/',
+        // target: 'http://192.168.168.8:33529/',
+
+        //secure: false,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 设置这个参数可以避免跨域
+        pathRewrite: {
+          //重写路径
+          '^/': ''
+        }
+      }
+    }, //代理
     // 启用 quiet 后，除了初始启动信息之外的任何内容都不会被打印到控制台
     // 和 FriendlyErrorsPlugin 配合食用更佳
     quiet: true
   },
-  plugins:[
-    new BundleAnalyzerPlugin.BundleAnalyzerPlugin(),
+  plugins: [
     new HtmlWebpackPlugin({
       title: 'vue3.0',
-      template: path.resolve(__dirname,'index.html'),
-      filename: "index.html",
-      inject:'body',
-
+      template: path.resolve(__dirname, 'index.html'),
+      filename: 'index.html',
+      inject: 'body'
     }),
     // new webpack.HotModuleReplacementPlugin()
     new FriendlyErrorsPlugin({
@@ -62,40 +74,42 @@ const devConfig = merge(webpackBaseConfig,{
       // },
     })
   ],
-  optimization:{
-     // 在可能的情况下确定每个模块的导出。
-     providedExports: true,
-     // 找到 chunk 中共享的模块，取出来生成单独的 chunk。
-     runtimeChunk: true,
-     // 给模块更有意义更方便调试的名称。这下二为Webpack5
-     moduleIds: 'named',
-     // 给 chunk 更有意义更方便调试的名称。
-     chunkIds: 'named',
+  optimization: {
+    // 在可能的情况下确定每个模块的导出。
+    providedExports: true,
+    // 找到 chunk 中共享的模块，取出来生成单独的 chunk。
+    runtimeChunk: true,
+    // 给模块更有意义更方便调试的名称。这下二为Webpack5
+    moduleIds: 'named',
+    // 给 chunk 更有意义更方便调试的名称。
+    chunkIds: 'named'
   }
-
 })
 
-
-module.exports = new Promise((resolve,reject) => {
+module.exports = new Promise((resolve, reject) => {
   portfinder.basePort = devConfig.devServer.port
-  portfinder.getPort((err,port) => {
+  portfinder.getPort((err, port) => {
     if (err) reject(err)
     else {
       devConfig.devServer.port = port
-      devConfig.plugins.push(new FriendlyErrorsPlugin({
-        // 清除控制台原有的信息
-        clearConsole: true,
-        compilationSuccessInfo: {
-          messages: [`Your application is running here: http://${devConfig.devServer.host}:${port}`]
-        },
-        onErrors: () => { console.log("your application defeat") }
-      }))
+      devConfig.plugins.push(
+        new FriendlyErrorsPlugin({
+          // 清除控制台原有的信息
+          clearConsole: true,
+          compilationSuccessInfo: {
+            messages: [
+              `Your application is running here: http://${devConfig.devServer.host}:${port}`
+            ]
+          },
+          onErrors: () => {
+            console.log('your application defeat')
+          }
+        })
+      )
       resolve(devConfig)
     }
   })
 })
-
-
 
 // module.exports = smp.wrap(devConfig)
 // module.exports = devConfig
--- webpack.prod.conf.js
+++ webpack.prod.conf.js
@@ -1,43 +1,56 @@
 'use strict'
 
 const webpack = require('webpack')
-const {merge} = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const webpackBaseConfig = require('./webpack.config')
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const prodConfig = {
   mode: 'production',
   // https://webpack.js.org/configuration/devtool/#production
-  devtool:'#source-map',
-  plugins:[
+  devtool: 'source-map',
+  plugins: [
+    new BundleAnalyzerPlugin.BundleAnalyzerPlugin(),
     new HtmlWebpackPlugin({
-      title: 'vue3.0',
-      template: path.resolve(__dirname,'index.html'),
-      filename: "index.html",
-      inject:'body',
+      title: '微站平台',
+      template: path.resolve(__dirname, 'index.html'),
+      filename: 'index.html',
+      inject: true,
+      favicon: './favicon.ico',
       // 详情：
       //https://github.com/kangax/html-minifier#options-quick-reference
-      minify: {
-        removeComments: true,       //去注释
-        collapseWhitespace: true,   //去空格
-        removeAttributeQuotes: true //去除属性引号
-      },
-
-    }),
+      minify: true
+      // chunksSortMode: 'dependency'
+    })
   ],
-  optimization:{
+  optimization: {
     // 告诉 webpack 确定和标记块，这些块是其他块的子集，当更大的块已经被加载时，不需要加载这些子集。
     flagIncludedChunks: true,
     // 告诉 webpack 找出一个模块的顺序，这可以使打包出来的入口 bundle.js 最小化。
-    occurrenceOrder: true,
+    // occurrenceOrder: true,
     // 确定每个模块下导出被使用的。
     usedExports: true,
+    //合并含有相同模块的 chunk
+    mergeDuplicateChunks: true,
     // 告诉 webpack 查找可以安全地连接到单个模块的模块图的片段。取决于优化。
     concatenateModules: true,
     // 使用 UglifyjsWebpackPlugin 进行代码压缩。
-    minimize: true
-
+    minimize: true,
+    minimizer: [
+      //webpack v5 自带最新的
+      new TerserPlugin({
+        parallel: true, // 可省略，默认开启并行
+        terserOptions: {
+          toplevel: true, // 最高级别，删除无用代码
+          ie8: true,
+          safari10: true
+        }
+      })
+    ]
   }
 }
 
